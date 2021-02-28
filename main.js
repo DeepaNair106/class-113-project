e251a8da114d7c17ed3a9abfd76f8c75c1b514be
DeepaNair106 <72653999+DeepaNair106@users.noreply.github.com>
2021-02-28 12:50:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,166 @@
+function setup(){
+    canvas = createCanvas(500, 400);
+    canvas.position(300,200);
+    video = createCapture(VIDEO);
+    video.hide();
+    
+
+}
+
+function draw(){
+    image(video, 180, 100, 200,200);
+    
+    
+
+    ellipse(37, 38, 60, 60);
+    stroke(52, 214, 255);
+    fill(52, 214, 255);
+
+    ellipse(97, 38, 60, 60);
+    stroke(52, 214, 255);
+    fill(52, 214, 255);
+
+    ellipse(157, 38, 60, 60);
+    stroke(52, 214, 255);
+    fill(52, 214, 255);
+
+    ellipse(217, 38, 60, 60);
+    stroke(52, 214, 255);
+    fill(52, 214, 255);
+
+    ellipse(277, 38, 60, 60);
+    stroke(52, 214, 255);
+    fill(52, 214, 255);
+
+    ellipse(337, 38, 60, 60);
+    stroke(52, 214, 255);
+    fill(52, 214, 255);
+
+    ellipse(397, 38, 60, 60);
+    stroke(52, 214, 255);
+    fill(52, 214, 255);
+
+    ellipse(457, 38, 60, 60);
+    stroke(52, 214, 255);
+    fill(52, 214, 255);
+
+    ellipse(37, 360, 60, 60);
+    stroke(52, 214, 255);
+    fill(52, 214, 255);
+
+    ellipse(97, 360, 60, 60);
+    stroke(52, 214, 255);
+    fill(52, 214, 255);
+
+    ellipse(157, 360, 60, 60);
+    stroke(52, 214, 255);
+    fill(52, 214, 255);
+
+    ellipse(217, 360, 60, 60);
+    stroke(52, 214, 255);
+    fill(52, 214, 255);
+
+    ellipse(277, 360, 60, 60);
+    stroke(52, 214, 255);
+    fill(52, 214, 255);
+
+    ellipse(337, 360, 60, 60);
+    stroke(52, 214, 255);
+    fill(52, 214, 255);
+
+    ellipse(397, 360, 60, 60);
+    stroke(52, 214, 255);
+    fill(52, 214, 255);
+
+    ellipse(457, 360, 60, 60);
+    stroke(0,0,0);
+    fill(0,0,0);
+
+    rect(10, 68, 55, 55);
+    stroke(0,0,0);
+    fill(0,0,0);
+
+    rect(10, 120, 55, 55);
+    stroke(0,0,0);
+    fill(0,0,0);
+
+    rect(10, 160, 55, 55);
+    stroke(0,0,0);
+    fill(0,0,0);
+
+    rect(10, 210, 55, 55);
+    stroke(0,0,0);
+    fill(0,0,0);
+
+    rect(10, 250, 55, 55);
+    stroke(0,0,0);
+    fill(0,0,0);
+
+    rect(10, 275, 55, 55);
+    stroke(0,0,0);
+    fill(0,0,0);
+
+    //
+
+
+    rect(430, 68, 55, 55);
+    stroke(0,0,0);
+    fill(0,0,0);
+
+    rect(430, 120, 55, 55);
+    stroke(0,0,0);
+    fill(0,0,0);
+
+    rect(430, 160, 55, 55);
+    stroke(0,0,0);
+    fill(0,0,0);
+
+    rect(430, 210, 55, 55);
+    stroke(0,0,0);
+    fill(0,0,0);
+
+    rect(430, 250, 55, 55);
+    stroke(0,0,0);
+    fill(0,0,0);
+
+    rect(430, 275, 55, 55);
+    stroke(52, 214, 255);
+    fill(52, 214, 255);
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+    
+    
+    
+    
+}
+
+
+
+function apply_filter(){}
+    
+
+function take_snapshot(){
+    save("selfie.png");
+}
